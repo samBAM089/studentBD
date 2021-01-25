24f409b869412090f49e243d30ce068f81938bde
Samuel Ngo <77593756+samBAM089@users.noreply.github.com>
2021-01-25 15:29:53 +0100

    Add equals and hashcode method (#1)

--- src/main/java/model/Student.java
+++ src/main/java/model/Student.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.Objects;
+
 public class Student {
 
     private String firstName;
@@ -38,16 +40,26 @@ public class Student {
         this.gender = gender;
     }
 
+
+    @Override
     public String toString() {
 
         String printStudentInfo = firstName + " " + matriculationNumber + " " + gender;
         return printStudentInfo;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return matriculationNumber == student.matriculationNumber && Objects.equals(firstName, student.firstName) && Objects.equals(gender, student.gender);
+    }
 
-lala
-
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, matriculationNumber, gender);
+    }
 }
 
 
--- src/test/java/StudentTester.java
+++ src/test/java/StudentTester.java
@@ -1,5 +1,7 @@
 import model.Student;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -7,7 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class StudentTester {
 
     @Test
-    public void constructorTest(){
+    public void constructorTest() {
         //given
         Student myStudent = new Student("Sam", 1, "male");
         //when
@@ -19,6 +21,33 @@ public class StudentTester {
 
     }
 
+    @Test
+    public void methodEqualsTester(){
+        Student samuelNgo = new Student("Sam", 1, "male");
+        Student samuelN = new Student("Sam", 1, "male");
 
+        boolean result = samuelNgo.equals(samuelN);
+
+        assertEquals(true, result);
+
+    }
+/*
+    @ParameterizedTest
+    @MethodSource
+
+    public void methodHAshcodeTester(){String firstName, int matriculationNumber, String gender, boolean expected){
+
+        boolean result = new Student().equals(new Student);
+
+        assertEquals(expected, result);
+    }
+    private static Stream<Arguments> methodHAshcodeTester(){
+        return Stream.of(
+                Arguments.of("Sam", 1, "male", true),
+                Arguments.of("Sam", 2, "male", false)
+
+        );
+    }
+*/
 
 }
