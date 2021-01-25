f020bad25468a5e3c88771078061ac46c806e12c
Samuel N <samuel.ngo@me.com>
2021-01-25 15:29:18 +0100

    Add equals and hashcode method

--- src/main/java/StudentMain.java
+++ src/main/java/StudentMain.java
@@ -3,7 +3,17 @@ import model.Student;
 public class StudentMain {
 
     public static void main(String[] args) {
-        Student SamuelNgo = new Student("Sam", 1, "male");
-        System.out.println(SamuelNgo);
+        Student samuelNgo = new Student("Sam", 1, "male");
+       Student samuelN = new Student( "Sam", 2, "male");
+
+               boolean sameStudent = samuelNgo.equals(samuelN);
+
+        System.out.println(sameStudent);
+
+        int idSamuelNgo = samuelNgo.hashCode();
+        System.out.println(idSamuelNgo);
+        System.out.println(samuelN.hashCode());
+
+
     }
 }
